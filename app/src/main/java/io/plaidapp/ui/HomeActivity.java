d32fd7453d9bbd6348dbc0f82a0bafd4d5d93778
Phong <p7157568@gmail.com>
2016-07-18 22:17:52 +0700

    feat - anim toolbar

--- app/src/main/java/io/plaidapp/ui/HomeActivity.java
+++ app/src/main/java/io/plaidapp/ui/HomeActivity.java
@@ -2,14 +2,78 @@ package io.plaidapp.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ActionMenuView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dhbk.android.plaidapp.R;
+import io.plaidapp.util.AnimUtils;
 
 public class HomeActivity extends Activity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        //toolbar.inflateMenu(R.menu.main);
+        setActionBar(toolbar);
+        if (savedInstanceState == null) {
+            animateToolbar();
+        }
     }
+
+
+    private void animateToolbar() {
+        // this is gross but toolbar doesn't expose it's children to animate them :(
+        View t = toolbar.getChildAt(0);
+        if (t != null && t instanceof TextView) {
+            TextView title = (TextView) t;
+
+            // fade in and space out the title.  Animating the letterSpacing performs horribly so
+            // fake it by setting the desired letterSpacing then animating the scaleX ¯\_(ツ)_/¯
+            title.setAlpha(0f);
+            title.setScaleX(0.8f);
+
+            title.animate()
+                    .alpha(1f)
+                    .scaleX(1f)
+                    .setStartDelay(300)
+                    .setDuration(900)
+                    .setInterpolator(AnimUtils.getFastOutSlowInInterpolator(this));
+        }
+        View amv = toolbar.getChildAt(1);
+        if (amv != null & amv instanceof ActionMenuView) {
+            ActionMenuView actions = (ActionMenuView) amv;
+            popAnim(actions.getChildAt(0), 500, 200); // filter
+            popAnim(actions.getChildAt(1), 700, 200); // overflow
+        }
+    }
+
+
+    private void popAnim(View v, int startDelay, int duration) {
+        if (v != null) {
+            v.setAlpha(0f);
+            v.setScaleX(0f);
+            v.setScaleY(0f);
+
+            v.animate()
+                    .alpha(1f)
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setStartDelay(startDelay)
+                    .setDuration(duration)
+                    .setInterpolator(AnimationUtils.loadInterpolator(this,
+                            android.R.interpolator.overshoot));
+        }
+    }
+
 }
